199d74ee5e2855057a7bca1ca1fdf1106f8259ff
sokelva <sokelva@mail.ru>
2023-06-26 11:16:29 +0300

    Добавлена функция handleSession().

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,15 +1,34 @@
-﻿// Запросим возраст пользователя и сохраним в переменную
-let age = prompt("Пожалуйста, введите ваш возраст");
+﻿/*
+* Функция для проверки и сохранения  данных пользователя
+* Также блокирует доступ к сайту лицам, не подтвердившим свой возраст
+*
+* */
 
-if (age >= 18) {
-    // Те, кто старше 18, увидят приветствие и будут направлены на сайт
-    alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
-}
-else {
-    // Если введенное число < 18, либо если введено не число -
-    // пользователь будет перенаправлен
-    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены!");
-    window.location.href = "https://www.consultant.ru/document/cons_doc_LAW_61798/"
+function handleSession() {
+
+    // создадим объект Map для хранения сессии
+    let session = new Map();
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+
+    // Запросим возраст пользователя и тоже сохраним
+    session.set("age", prompt("Пожалуйста, введите ваш возраст"))
+
+    // Проверка на возраст и сохранение сессии
+    if (session.get("age") >= 18) {
+        let startDate = new Date().toLocaleString();
+
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+        session.set("startDate", startDate)
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
 }
 
 /*
