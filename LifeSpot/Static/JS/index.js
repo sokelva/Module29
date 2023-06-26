6a667be81642f693a80da4a1fcc86e5897821d8e
sokelva <sokelva@mail.ru>
2023-06-26 11:10:57 +0300

    Добавлен функция JS для фильтрации контента на странице сайта.

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -11,3 +11,32 @@ else {
     alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены!");
     window.location.href = "https://www.consultant.ru/document/cons_doc_LAW_61798/"
 }
+
+/*
+* Функция для фильтрации контента.
+* Будет вызываться благодаря атрибуту oninput на index.html
+*/
+
+function getFilter() {
+    // Сохраняем текст пользовательского запроса.
+    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+    // Находим контейнеры с видео, которые необходимо фильтровать
+    let elements = document.getElementsByClassName('video-container');
+
+    // Пробегаемся по контейнерам
+    for (let i = 0; i <= elements.length; i++) {
+        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
+        let videoText = elements[i].querySelector(".video-title").innerText;
+        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
+        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
+            // Описание
+            elements[i].style.display = 'none';
+        }
+        else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+
+
+           
