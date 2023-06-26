39b50d6cb3f34e698e416eae96deb3dab498c4b6
sokelva <sokelva@mail.ru>
2023-06-26 14:25:39 +0300

    Изменено Логирование сессии (объявлено через function expression).

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,18 +1,20 @@
-﻿/*
-* Функция для проверки и сохранения  данных пользователя
-* Также блокирует доступ к сайту лицам, не подтвердившим свой возраст
-*
-* */
+﻿// Логирование сессии (объявлено через expression)
+let sessionLog = function logSession(session) {
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
+}
 
+// Обработка сессии (объявлено через declaration)
 function handleSession() {
-
     // создадим объект Map для хранения сессии
     let session = new Map();
     // Сохраним UserAgent
     session.set("userAgent", window.navigator.userAgent)
 
     // Запросим возраст пользователя и тоже сохраним
-    session.set("age", prompt("Пожалуйста, введите ваш возраст"))
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
 
     // Проверка на возраст и сохранение сессии
     if (session.get("age") >= 18) {
@@ -25,10 +27,9 @@ function handleSession() {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
         window.location.href = "http://www.google.com"
     }
-    // Вывод в консоль
-    for (let result of session) {
-        console.log(result)
-    }
+
+    // Теперь мы возвращаем объект сессии
+    return session;
 }
 
 /*
